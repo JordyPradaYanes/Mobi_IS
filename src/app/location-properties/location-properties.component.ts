e2629014fb66aff00f8d3cda2cd6bd58b8ae20f4
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-04-05 23:30:08 -0500

    Add files location-properties and property-benefits

---
+++ src/app/location-properties/location-properties.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-properties',
+  imports: [],
+  templateUrl: './location-properties.component.html',
+  styleUrl: './location-properties.component.css'
+})
+export class LocationPropertiesComponent {
+
+}
---
+++ src/app/property-benefits/property-benefits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-benefits',
+  imports: [],
+  templateUrl: './property-benefits.component.html',
+  styleUrl: './property-benefits.component.css'
+})
+export class PropertyBenefitsComponent {
+
+}
